4ac8d25016959ab7c27a01e9922c60762b698bda
HuYueling <1228205445@qq.com>
2017-08-21 19:53:51 +0800

    登录代码

---
+++ GetHotelsEasy/GetHotelsEasy/Controller/LoginViewController.m
@@ -0,0 +1,48 @@
+//
+//  LoginViewController.m
+//  GetHotelsEasy
+//
+//  Created by admin on 2017/8/21.
+//  Copyright © 2017年 self. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UITextField *phoneTextField;
+@property (weak, nonatomic) IBOutlet UITextField *pwdTextField;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+- (IBAction)loginAction:(UIButton *)sender forEvent:(UIEvent *)event;
+@property (weak, nonatomic) IBOutlet UIButton *createUserBtn;
+- (IBAction)createUserAction:(UIButton *)sender forEvent:(UIEvent *)event;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)loginAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+- (IBAction)createUserAction:(UIButton *)sender forEvent:(UIEvent *)event {
+}
+@end
