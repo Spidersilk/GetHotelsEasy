465c772391ebb75e0d5f20dac4356e451d7115ac
PengJinPing <2468814340@qq.com>
2017-08-21 21:25:27 +0800

    注册页

---
+++ GetHotelsEasy/GetHotelsEasy/Controller/CreateUserViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateUserViewController.h
+//  GetHotelsEasy
+//
+//  Created by admin on 2017/8/21.
+//  Copyright © 2017年 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateUserViewController : UIViewController
+
+@end
