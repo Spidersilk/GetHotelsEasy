fdfee49600a68dfcd72c0310a1e4ba11b6983bce
ZILONG <571099073@qq.com>
2017-08-24 10:29:37 +0800

    设置详情页导航栏

--- GetHotelsEasy/GetHotelsEasy/Controller/HotelOrdelViewController.m
+++ GetHotelsEasy/GetHotelsEasy/Controller/HotelOrdelViewController.m
@@ -45,7 +45,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+    [self naviConfing];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -58,8 +58,34 @@
     [self.navigationController setNavigationBarHidden:NO animated:NO];
     
 }
-
-
+- (void)naviConfing
+{
+    //self.navigationController.navigationBar.backgroundColor = [UIColor blueColor];
+    //设置导航条的风格颜色
+    self.navigationController.navigationBar.barTintColor = UIColorFromRGB(1, 150, 255);
+    //设置导航条标题颜色
+    self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName:[UIColor whiteColor]};
+    //设置导航条是否被隐藏
+    self.navigationController.navigationBar.hidden = NO;
+    //设置导航条按钮的分格颜色
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    //是否需要毛玻璃的效果
+    self.navigationController.navigationBar.translucent = YES;
+    //实例化一个button 类型为UIButtonTypeSystem
+    UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    //设置位置大小
+    leftBtn.frame = CGRectMake(0, 0, 22, 22);
+    //设置其背景图片为返回图片
+    [leftBtn setBackgroundImage:[UIImage imageNamed:@"返回"] forState:UIControlStateNormal];
+    //给按钮添加事件
+    [leftBtn addTarget:self action:@selector(leftButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftBtn];
+}
+//自定的返回按钮的事件
+- (void)leftButtonAction: (UIButton *)sender{
+    [self.navigationController popViewControllerAnimated:YES];
+}
 /*
 #pragma mark - Navigation
 
