9c105e3ae1e8e9a4f5dfb495f8486cbc7b64232f
余英鑫 <442245709@qq.com>
2017-09-19 09:19:21 +0800

    picker动画

--- GetHotelsEasy/GetHotelsEasy/Controller/CityTableViewController.m
+++ GetHotelsEasy/GetHotelsEasy/Controller/CityTableViewController.m
@@ -43,6 +43,10 @@
 //将要来到此页面（隐藏导航栏）
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
+    
+    //去除导航栏半透膜
+    self.navigationController.navigationBar.translucent = NO;
+    
     [self.navigationController setNavigationBarHidden:NO animated:NO];
 }
 - (void)didReceiveMemoryWarning {
@@ -52,7 +56,8 @@
 //这个方法专门做导航条的控制
 - (void)naviConfig{
     //设置导航条的颜色（风格颜色）
-    self.navigationController.navigationBar.barTintColor = [UIColor grayColor];
+    self.navigationController.navigationBar.barTintColor = UIColorFromRGB(14, 123, 235);
+
     //设置导航条标题颜色
     self.navigationController.navigationBar.titleTextAttributes = @{NSForegroundColorAttributeName : [UIColor whiteColor]};
     //设置导航条是否被隐藏
@@ -61,7 +66,7 @@
     //设置导航条上按钮的风格颜色
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     //设置是否需要毛玻璃效果
-    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.navigationBar.translucent = NO;
     //为导航条左上角创建一个按钮
     UIBarButtonItem *left = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemStop target:self action:@selector(backAction)];
     self.navigationItem.leftBarButtonItem = left;
